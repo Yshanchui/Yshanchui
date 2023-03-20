ab9fb6dee7c1cb0603f61f64734ad95729183fef
ShanChui <qizhidexiaomimei@gmail.com>
2023-03-20 11:26:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from datetime import datetime
 
 def main():
 
-    new_str = "😄"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d') + "😄"
+    new_str = "✨"+  datetime.fromtimestamp(int(time.time()),pytz.timezone('Asia/Shanghai')).strftime('%Y-%m-%d') + "✨"
     # 获取README.md内容
     with open (os.path.join(os.getcwd(), "README.md"), 'r', encoding='utf-8') as f:
         readme_md_content = f.read()
 
     print(new_str)
 
-    new_readme_md_content = re.sub(r'😄(.|\n)*😄', new_str, readme_md_content)
+    new_readme_md_content = re.sub(r'✨(.|\n)*✨', new_str, readme_md_content)
 
     with open (os.path.join(os.getcwd(), "README.md"), 'w', encoding='utf-8') as f:
         f.write(new_readme_md_content)
